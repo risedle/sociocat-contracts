110a884d01e0d066c84e3f92590bb251a1443109
farreldarian <farreldarian@users.noreply.github.com>
2023-10-08 16:41:48 +0700

    Reformat and Update Solidity Version

--- src/SociocatAirdrop.sol
+++ src/SociocatAirdrop.sol
@@ -1,46 +1,47 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.21;
+pragma solidity ^0.8.17;
 
 import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
-import {MerkleProof} from "@openzeppelin/contracts/utils/cryptography/MerkleProof.sol";
-import {ReentrancyGuard} from "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
+import {MerkleProof} from
+  "@openzeppelin/contracts/utils/cryptography/MerkleProof.sol";
+import {ReentrancyGuard} from
+  "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
 
 error SociocatAirdrop_InvalidProof();
 error SociocatAirdrop_AlreadyClaimed();
 error SociocatAirdrop_InvalidParams();
 
 contract SociocatAirdrop is ReentrancyGuard {
-    IERC20 public token;
-    bytes32 public root;
-    mapping(address => bool) public claimed;
+  IERC20 public token;
+  bytes32 public root;
+  mapping(address => bool) public claimed;
 
-    event Claimed(address indexed receiver, uint256 amount);
+  event Claimed(address indexed receiver, uint256 amount);
 
-    constructor(IERC20 _token, bytes32 _root) {
-        if (address(_token) == address(0) || _root == 0) {
-            revert SociocatAirdrop_InvalidParams();
-        }
-
-        token = _token;
-        root = _root;
+  constructor(IERC20 _token, bytes32 _root) {
+    if (address(_token) == address(0) || _root == 0) {
+      revert SociocatAirdrop_InvalidParams();
     }
 
-    function claim(
-        uint256 amount,
-        bytes32[] calldata proof
-    ) external nonReentrant {
-        bytes32 leaf = keccak256(
-            bytes.concat(keccak256(abi.encode(msg.sender, amount)))
-        );
-        if (!MerkleProof.verify(proof, root, leaf)) {
-            revert SociocatAirdrop_InvalidProof();
-        }
-        if (claimed[msg.sender]) {
-            revert SociocatAirdrop_AlreadyClaimed();
-        }
+    token = _token;
+    root = _root;
+  }
 
-        claimed[msg.sender] = true;
-        token.transfer(msg.sender, amount);
-        emit Claimed(msg.sender, amount);
+  function claim(uint256 amount, bytes32[] calldata proof)
+    external
+    nonReentrant
+  {
+    bytes32 leaf =
+      keccak256(bytes.concat(keccak256(abi.encode(msg.sender, amount))));
+    if (!MerkleProof.verify(proof, root, leaf)) {
+      revert SociocatAirdrop_InvalidProof();
     }
+    if (claimed[msg.sender]) {
+      revert SociocatAirdrop_AlreadyClaimed();
+    }
+
+    claimed[msg.sender] = true;
+    token.transfer(msg.sender, amount);
+    emit Claimed(msg.sender, amount);
+  }
 }
--- test/SociocatAirdrop.t.sol
+++ test/SociocatAirdrop.t.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.21;
+pragma solidity ^0.8.17;
 
 import "../src/SociocatAirdrop.sol";
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
@@ -16,80 +16,81 @@ import "forge-std/Test.sol";
 // ];
 // const tree = StandardMerkleTree.of(values, ["address", "uint256"]);
 // ```
-bytes32 constant root = 0xdd9d071d0ae3c1e1e55dd4865b8a6d3f72f08af272fe0831fa6ead525d6ad710;
+bytes32 constant root =
+  0xdd9d071d0ae3c1e1e55dd4865b8a6d3f72f08af272fe0831fa6ead525d6ad710;
 
 contract SociocatAirdropTest is Test {
-    SociocatAirdrop airdrop;
-    TestToken token;
+  SociocatAirdrop airdrop;
+  TestToken token;
 
-    function setUp() public {
-        token = new TestToken();
-        airdrop = new SociocatAirdrop(token, root);
-        token.mint(address(airdrop), 3 ether);
-    }
+  function setUp() public {
+    token = new TestToken();
+    airdrop = new SociocatAirdrop(token, root);
+    token.mint(address(airdrop), 3 ether);
+  }
 
-    /** ——— constructor() */
+  /**
+   * ——— constructor()
+   */
 
-    function test_rejectsInvalidToken() public {
-        vm.expectRevert(SociocatAirdrop_InvalidParams.selector);
-        new SociocatAirdrop(IERC20(address(0)), root);
-    }
+  function test_rejectsInvalidToken() public {
+    vm.expectRevert(SociocatAirdrop_InvalidParams.selector);
+    new SociocatAirdrop(IERC20(address(0)), root);
+  }
 
-    function test_rejectsInvalidRoot() public {
-        vm.expectRevert(SociocatAirdrop_InvalidParams.selector);
-        new SociocatAirdrop(token, 0x0);
-    }
+  function test_rejectsInvalidRoot() public {
+    vm.expectRevert(SociocatAirdrop_InvalidParams.selector);
+    new SociocatAirdrop(token, 0x0);
+  }
 
-    /** ——— claim() */
+  /**
+   * ——— claim()
+   */
 
-    function test_rejectsInvalidProof() public {
-        vm.expectRevert(SociocatAirdrop_InvalidProof.selector);
-        airdrop.claim(1, new bytes32[](0));
-    }
+  function test_rejectsInvalidProof() public {
+    vm.expectRevert(SociocatAirdrop_InvalidProof.selector);
+    airdrop.claim(1, new bytes32[](0));
+  }
 
-    function test_claims() public {
-        address vitalik = 0xd8dA6BF26964aF9D7eEd9e03E53415D37aA96045;
-        bytes32[] memory proof = new bytes32[](2);
-        proof[
-            0
-        ] = 0xb7790ece4442bbc8f93fc4b7b6aa37bb39d2971048f98e64a3190c76c82e2381;
-        proof[
-            1
-        ] = 0xebecbbb383635879d8415e0628c3d0b82dcee85319864be9aead45d0d120f5cb;
+  function test_claims() public {
+    address vitalik = 0xd8dA6BF26964aF9D7eEd9e03E53415D37aA96045;
+    bytes32[] memory proof = new bytes32[](2);
+    proof[0] =
+      0xb7790ece4442bbc8f93fc4b7b6aa37bb39d2971048f98e64a3190c76c82e2381;
+    proof[1] =
+      0xebecbbb383635879d8415e0628c3d0b82dcee85319864be9aead45d0d120f5cb;
 
-        assertEq(token.balanceOf(vitalik), 0); // Check initial balance
+    assertEq(token.balanceOf(vitalik), 0); // Check initial balance
 
-        vm.prank(vitalik);
-        airdrop.claim(1 ether, proof);
+    vm.prank(vitalik);
+    airdrop.claim(1 ether, proof);
 
-        assertEq(token.balanceOf(vitalik), 1 ether); // Should receive the token
-    }
+    assertEq(token.balanceOf(vitalik), 1 ether); // Should receive the token
+  }
 
-    function test_rejectsDoubleClaim() public {
-        address vitalik = 0xd8dA6BF26964aF9D7eEd9e03E53415D37aA96045;
-        bytes32[] memory proof = new bytes32[](2);
-        proof[
-            0
-        ] = 0xb7790ece4442bbc8f93fc4b7b6aa37bb39d2971048f98e64a3190c76c82e2381;
-        proof[
-            1
-        ] = 0xebecbbb383635879d8415e0628c3d0b82dcee85319864be9aead45d0d120f5cb;
+  function test_rejectsDoubleClaim() public {
+    address vitalik = 0xd8dA6BF26964aF9D7eEd9e03E53415D37aA96045;
+    bytes32[] memory proof = new bytes32[](2);
+    proof[0] =
+      0xb7790ece4442bbc8f93fc4b7b6aa37bb39d2971048f98e64a3190c76c82e2381;
+    proof[1] =
+      0xebecbbb383635879d8415e0628c3d0b82dcee85319864be9aead45d0d120f5cb;
 
-        // First claim
-        vm.prank(vitalik);
-        airdrop.claim(1 ether, proof);
+    // First claim
+    vm.prank(vitalik);
+    airdrop.claim(1 ether, proof);
 
-        // Second claim
-        vm.expectRevert(SociocatAirdrop_AlreadyClaimed.selector);
-        vm.prank(vitalik);
-        airdrop.claim(1 ether, proof);
-    }
+    // Second claim
+    vm.expectRevert(SociocatAirdrop_AlreadyClaimed.selector);
+    vm.prank(vitalik);
+    airdrop.claim(1 ether, proof);
+  }
 }
 
 contract TestToken is ERC20 {
-    constructor() ERC20("Test Token", "TEST") {}
+  constructor() ERC20("Test Token", "TEST") {}
 
-    function mint(address to, uint256 amount) external {
-        _mint(to, amount);
-    }
+  function mint(address to, uint256 amount) external {
+    _mint(to, amount);
+  }
 }
