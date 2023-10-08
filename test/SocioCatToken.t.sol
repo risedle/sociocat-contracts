3159e1c7c206be04db6c262fccb424504c1e8ca5
Bayu Aldi Yansyah <bayualdiyansyah@gmail.com>
2023-10-08 19:53:59 +0700

    chore: initialize token

---
+++ test/SocioCatToken.t.sol
@@ -0,0 +1,7 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.17;
+
+import {Test} from "forge-std/Test.sol";
+import {SocioCatToken} from "../src/SocioCatToken.sol";
+
+contract SocioCatToken is Test {}
