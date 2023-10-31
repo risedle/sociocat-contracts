af0c21b8f998586dffb58a4fe2e2a0ac7e8aa972
farreldarian <farreldarian@users.noreply.github.com>
2023-10-31 10:50:13 +0700

    Update deployment script

--- script/DeployAirdrop.s.sol
+++ script/DeployAirdrop.s.sol
@@ -9,6 +9,8 @@ import {SocioCatAirdrop} from "../src/SocioCatAirdrop.sol";
 contract DeployTestAirdrop is Script {
   address treasury = 0x08B5C677f6BF699425Dd5594Dce28473F8EFF823;
   bytes32 root = 0xe4eaf9c0c461a2f5ec0074f2726ec473ecb327e59b32c0c60ca08a3f8f4a1544;
+  // TODO SET REAL VALUE
+  uint256 claimEndTime = 1_701_277_200; // 30 November 2023
 
   function run() public returns (SocioCatToken token, SocioCatAirdrop airdrop) {
     uint256 deployerPrivateKey = uint256(vm.envBytes32("PRIVATE_KEY"));
@@ -17,7 +19,9 @@ contract DeployTestAirdrop is Script {
     token = new SocioCatToken(treasury, "SocioCat", "CAT");
     airdrop = new SocioCatAirdrop(
       token,
-      root
+      root,
+      treasury,
+      claimEndTime
     );
   }
 }
